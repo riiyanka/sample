fa5debcf2873adcdda78a8e51a53bb16da005e8c
riiyanka <riyankadey07@gmail.com>
2025-08-29 16:08:33 +0530

    Remove all files from riyanka_branch

--- frontend/src/pages/ActiveFormsPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function ActiveFormsPage() {
-  return <h1>Current Active Forms (Placeholder)</h1>;
-}
-
-export default ActiveFormsPage;
--- frontend/src/pages/AnalyticsPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function AnalyticsPage() {
-  return <h1>Analytics Dashboard (Placeholder)</h1>;
-}
-
-export default AnalyticsPage;
